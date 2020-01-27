99adff1f110bd271e493f3e12e2574f7fae99ebf
Aliaksei Shalak <aliaksei.shalak@rednavis.com>
2020-01-27 17:35:13 +0300

    Initial version of microservice generated by JHipster-6.6.0

---
+++ src/main/java/com/terabiz/microservice/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.terabiz.microservice.config.audit;
---
+++ src/main/java/com/terabiz/microservice/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.terabiz.microservice.web.rest.vm;
